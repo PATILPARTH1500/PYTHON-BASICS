226d9286df883c7d3dc4c2d13626f6e73c7a796c
Parth Patil <patilparth150@gmail.com>
2025-01-16 15:35:48 +0530

    created main.py for dictionary and set

---
+++ DICT AND SETS/main.py
@@ -0,0 +1,11 @@
+# Dictionary in Python 
+# Dictionaries are use to store data values in key:value pairs
+# They are unordered, mutable(changeable), dont allow duplicate keys
+
+info = {
+    "name" : "Parth Patil",
+    "cgpa" : 9.5,
+    "marks" : [97, 98, 93],
+    "is_adult" : True,
+}
+print(info) #Output {'name': 'Parth Patil', 'cgpa': 9.5, 'marks': [97, 98, 93]}
