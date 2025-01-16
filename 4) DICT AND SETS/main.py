b5380f3a114099f51ddcece55a8c19238afef615
Parth Patil <patilparth150@gmail.com>
2025-01-17 05:22:56 +0530

    done with the dict and sets

--- 4) DICT AND SETS/main.py
+++ 4) DICT AND SETS/main.py
@@ -75,4 +75,75 @@
 
 #Sets in Python
 #Set is the collection of the unordered items
-#Each elements in the set must be unique & immutable
+#Each elements in the set must be unique & immutable also this statement says that elements of the set is immutable not the set 
+#Set is mutable
+#List and Dictionary cannot be in the sets beacuse they are muttable
+#List and Dictionary are unhashable because there hash values can be changed 
+
+# collection = {1, 2, 3, 4, 2, 3, "Hello" , "World" , "Hello"}
+# print(collection) #Output {1, 2, 3, 4, 'Hello', 'World'} It will always ignore the duplicate elements in the set
+# print(len(collection)) #Output 6 Length function will also ignore the duplicate elements of the sets
+# print(type(collection)) #Output <class 'set'>
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Empty Set
+# null = set() # This is an empty set
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Sets Method
+
+#Add Method
+# collection = set()
+# collection.add(12)
+# collection.add("parth")
+# collection.add((1, 2, 3, 4))
+# print(collection) #Output {(1, 2, 3, 4), 12, 'parth'}
+# print(len(collection)) #It only counts the element type not the actual element #Output 3
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# #Remove Method 
+# collection.remove(12)
+# print(collection) #Output {(1, 2, 3, 4), 'parth'}
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Clear Method
+# collection.clear()
+# print(collection) #Output set() it gives an empty set
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+#Pop Method
+#collection.pop() Randomly picks any value from the above set
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# #Union Method
+# collection1 = {1, 2, 3, 4, 5}
+# collection2 = {5, 4, 3, 2, 1}
+# collection1.union(collection2)
+# print(collection1) #Output {1, 2, 3, 4, 5}
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# #Intersection Method
+# collection1 = {1, 2, 3, 4, 5}
+# collection2 = {6, 7, 4, 3, 2,}
+# collection1.intersection(collection2)
+# print(collection1)
+#--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Quiz
+
+# Write a python program to store following word meanings in the python dictionary
+#table : "a piece of furniture" , "list of facts & figures"
+#cat : "a small animal"
+
+# quiz = {
+#     "table"  : "a piece of furniture", "List of facts & figures"
+#     "cat" : "a small animal"
+# }
+# print(quiz)
+
+#Write a python program you are given a list for students. Assume one classroom is requried for 1 subjects How many classrooms are needed 
+# all students
+
+# subjects = {"python", "java", "C++", "python", "javascript", "java", "python", "java", "C++", "C" }
+# print(len(subjects))
+# ans is 5
