f4f7619a1451d8cec8aec48f61d3f7bc41c76417
Parth Patil <patilparth150@gmail.com>
2025-01-16 15:55:16 +0530

    donw till nested loops and getting error dont know why

--- DICT AND SETS/main.py
+++ DICT AND SETS/main.py
@@ -2,10 +2,34 @@
 # Dictionaries are use to store data values in key:value pairs
 # They are unordered, mutable(changeable), dont allow duplicate keys
 
+# info = {
+#     "name" : "Parth Patil",
+#     "cgpa" : 9.5,
+#     "marks" : [97, 98, 93],
+#     "is_adult" : True,
+# }
+# print(info) #Output {'name': 'Parth Patil', 'cgpa': 9.5, 'marks': [97, 98, 93]}
+# print(info["name"]) #Output Parth Patil
+# info["name"] = "Rudresh Patil"
+# info["name"] = "Daksh Patil"
+# print(info["name"])
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+# Null Dictionary
+# null = {}
+# print(type(null))
+
+#-----------------------------------------------------------------------------------------------------------------------------------------
+
+# Nested Dictionary 
+
 info = {
     "name" : "Parth Patil",
-    "cgpa" : 9.5,
-    "marks" : [97, 98, 93],
-    "is_adult" : True,
+    "marks" : {
+        "Chem" : 30,
+        "Phy"  : 30,
+        "maths" : 80,}
+    "age" : 18,
+    "is_passed" : True,
 }
-print(info) #Output {'name': 'Parth Patil', 'cgpa': 9.5, 'marks': [97, 98, 93]}
+print(info)
