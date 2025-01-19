e7b8cac6f08765bf34e79f3416d3e46d23bbe179
Parth Patil <patilparth150@gmail.com>
2025-01-20 01:38:36 +0530

    created function and recursion folder

---
+++ FUNCTIONS AND RECURSION/main.py
@@ -0,0 +1,13 @@
+#FUNCTION IN PYTHON
+#BLOCK OF STATEMENT THAT PERFORMS A SPECIFIC TASK
+#REPEAT = REDUNDANT AVOID WRITING SAME CODE 
+#ALWAYS PREFER FUNCTION FOR REDUNDANT THINGS
+
+def calc_sum(a, b): #defining function 
+    sum = a + b #what is the purpose of the variables in function ?
+    print(sum) #printing sum
+    return sum #shows output
+    
+calc_sum(2, 4) #calling function
+calc_sum(4, 1)
+calc_sum(4, 7)
